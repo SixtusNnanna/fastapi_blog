1a1bced79181ad19a0e307fb9a54e661ef307637
Sixtus Omeje <117023564+SixtusNnanna@users.noreply.github.com>
2026-02-21 16:36:40 +0100

    Include POSTGRES_PASSWORD in DATABASE_URL
    Updated DATABASE_URL to include POSTGRES_PASSWORD.

--- backend/core/config.py
+++ backend/core/config.py
@@ -13,6 +13,7 @@ class Settings:
     PROJECT_TITLE: str = "Blog"
     PROJECT_VERSION: str = "0.1.0"
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
     POSTGRES_PORT: int = os.getenv("POSTGRES_PORT", 5432)
     POSTGRES_DB: str = os.getenv("POSTGRES_DB")
@@ -21,7 +22,7 @@ class Settings:
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     DATABASE_URL = (
-        f"postgresql://{POSTGRES_USER}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
     )
 
 
